528917c7224efd497411a913f4aa5ccd2b865819
Hyun Woo Park <phu54321@naver.com>
2020-07-13 00:49:20 +0900

    Squashed merge from template/jsinterop (2020-07-13)

--- src/__init__.py
+++ src/__init__.py
@@ -19,24 +19,30 @@ from .utils.configrw import getConfig
 from .utils.resource import readResource
 from .utils.JSCallable import JSCallable
 
+
 @JSCallable
 def isActiveWindowAnki():
     return aqt.mw.app.activeWindow() != None
 
+
 def afterInitWeb(self):
-    js = readResource('js/main.min.js')
-    self.web.settings().setAttribute(QWebEngineSettings.PlaybackRequiresUserGesture, False)
+    js = readResource("js/main.min.js")
+    self.web.settings().setAttribute(
+        QWebEngineSettings.PlaybackRequiresUserGesture, False
+    )
+
     def cb2(res):
-        if getConfig('alarmFile'):
-            fname = getConfig('alarmFile')
-            with open(fname, 'rb') as f:
+        if getConfig("alarmFile"):
+            fname = getConfig("alarmFile")
+            with open(fname, "rb") as f:
                 content = f.read()
             mimetype = mimetypes.guess_type(fname)[0]
             if mimetype:
-                b64 = base64.b64encode(content).decode('ascii')
-                dataURI = f'data:{mimetype};base64,{b64}'
+                b64 = base64.b64encode(content).decode("ascii")
+                dataURI = f"data:{mimetype};base64,{b64}"
                 self.web.eval(f'window._atSetAlarmSoundUrl("{dataURI}")')
 
     self.web.evalWithCallback(js, cb2)
+
 
 Reviewer._initWeb = wrap(Reviewer._initWeb, afterInitWeb, "after")
