21555dc5f8e54fa2c02c0026b00e84ccd5181135
Hyun Woo Park <phu54321@naver.com>
2020-07-14 15:02:36 +0900

    :bug: removed console.log on idle.ts
    maybe I should find a way of enforcing 'no-console' rule on pre-commit hook :(

--- jssrc/idle.ts
+++ jssrc/idle.ts
@@ -3,7 +3,6 @@ let t: number | null = null
 let _isIdle = false
 
 function onIdleTimer () {
-  console.log('onIdleTimer')
   _isIdle = true
   t = null
 }
@@ -12,7 +11,6 @@ function resetIdleTimer () {
   _isIdle = false
   if (t !== null) clearTimeout(t)
   t = setTimeout(onIdleTimer, 1000)
-  console.log('resetIdleTimer')
 }
 
 for (const ev of events) {
