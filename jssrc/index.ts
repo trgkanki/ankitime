277a3c38005760fdfed4f57eb75bf592005f3c0d
Hyun Woo Park <phu54321@naver.com>
2020-08-30 02:55:28 +0900

    🔥 removed console.logs

--- jssrc/index.ts
+++ jssrc/index.ts
@@ -13,9 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-console.log(1)
 if (!window._atInstance) {
-  console.log(2)
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { ATInstance } = require('./atInstance')
   window._atInstance = new ATInstance()
