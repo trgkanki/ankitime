a23fdfeef5c4eeaf9ee600190b36027fcacc7f32
Hyun Woo Park <phu54321@naver.com>
2020-08-30 16:51:05 +0900

    ✨ reset to default alarm when no alarmFile set

--- src/__init__.py
+++ src/__init__.py
@@ -24,6 +24,7 @@
 from aqt.reviewer import Reviewer
 from anki.hooks import wrap, addHook
 from aqt.qt import QWebEngineSettings
+from aqt.utils import showInfo
 from aqt import AnkiQt
 import aqt
 import base64
@@ -50,7 +51,6 @@ def afterInitWeb(self):
     self.web.settings().setAttribute(
         QWebEngineSettings.PlaybackRequiresUserGesture, False
     )
-    updateMedia("_at_alert.mp3", readResource("sfx/alert.mp3", binary=True), False)
     updateMedia("_at_resume.mp3", readResource("sfx/resume.mp3", binary=True), False)
 
     def cb2(res):
@@ -58,19 +58,30 @@ def afterInitWeb(self):
             self.web.eval("window._atInstance.enableIdleAlarm(true)")
 
         fname = getConfig("alarmFile")
-        if fname and os.path.isfile(fname):
-            with open(fname, "rb") as f:
-                content = f.read()
-            mimetype = mimetypes.guess_type(fname)[0]
+        if fname:
+            updateMedia(
+                "_at_alert.mp3", readResource("sfx/alert.mp3", binary=True), False
+            )
+            if os.path.isfile(fname):
+                with open(fname, "rb") as f:
+                    content = f.read()
+                mimetype = mimetypes.guess_type(fname)[0]
 
-            if mimetype == "audio/mpeg":
-                updateMedia("_at_alert.mp3", content)
-                # Reload _at_alert.mp3 on JS side
-                self.web.eval(f'window._atInstance.setAlarmSound("_at_alert.mp3")')
+                if mimetype == "audio/mpeg":
+                    updateMedia("_at_alert.mp3", content)
+                    # Reload _at_alert.mp3 on JS side
+                    self.web.eval(f'window._atInstance.setAlarmSound("_at_alert.mp3")')
+                else:
+                    b64 = base64.b64encode(content).decode("ascii")
+                    dataURI = f"data:{mimetype};base64,{b64}"
+                    self.web.eval(f'window._atInstance.setAlarmSound("{dataURI}")')
             else:
-                b64 = base64.b64encode(content).decode("ascii")
-                dataURI = f"data:{mimetype};base64,{b64}"
-                self.web.eval(f'window._atInstance.setAlarmSound("{dataURI}")')
+                showInfo('[Anki Time] unknown file "%s"' % fname)
+
+        else:
+            updateMedia(
+                "_at_alert.mp3", readResource("sfx/alert.mp3", binary=True), True
+            )
 
     self.web.evalWithCallback(js, cb2)
 
